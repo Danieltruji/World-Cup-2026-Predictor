613f111dd37ea03e8eb1eb717dcdd47e95f46e8c
Danny <danieltruji0428@gmail.com>
2025-06-18 00:44:05 -0400

    updated group brackets in club world cup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Welcome from './pages/home';
 import Teams from './pages/teams';
 import TeamDetailPage from './pages/teamDetailPages';
 import ClubWorldCup from './pages/clubWorldCup';
+import ClubWorldCupPredict from './pages/clubWorldCupPredict';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/teams" element={<Teams/>} />
         <Route path="/teams/:teamId" element={<TeamDetailPage />} />
         <Route path="/club-world-cup" element={<ClubWorldCup />} />
+        <Route path="/club-world-cup/predict" element={<ClubWorldCupPredict />} />
         {/* <Route path="/predict" element={<Predict />} /> */}
         {/* You can add more routes later */}
       </Routes>
--- frontend/src/pages/clubWorldCup.js
+++ frontend/src/pages/clubWorldCup.js
@@ -2,6 +2,57 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../pages/stylesheets/clubWorldCup.css'
 
+const groupedTeams = {
+  "Group A": [
+    "SE Palmeiras (BRA)",
+    "FC Porto (POR)",
+    "Al Ahly FC (EGY)",
+    "Inter Miami CF (USA)"
+  ],
+  "Group B": [
+    "Paris St. Germain (FRA)",
+    "Atlético Madrid (ESP)",
+    "Botafogo (BRA)",
+    "Seattle Sounders (USA)"
+  ],
+  "Group C": [
+    "Bayern München (GER)",
+    "Auckland City FC (NZL)",
+    "CA Boca Juniors (ARG)",
+    "SL Benfica (POR)"
+  ],
+  "Group D": [
+    "CR Flamengo (BRA)",
+    "Espérance Tunis (TUN)",
+    "Chelsea FC (ENG)",
+    "Club León (MEX)"
+  ],
+  "Group E": [
+    "CA River Plate (ARG)",
+    "Urawa Reds D. (JPN)",
+    "CF Monterrey (MEX)",
+    "FC Inter Milan (ITA)"
+  ],
+  "Group F": [
+    "Fluminense FC (BRA)",
+    "Borussia Dortmund (GER)",
+    "Ulsan HD FC (KOR)",
+    "Mamelodi Sundowns (RSA)"
+  ],
+  "Group G": [
+    "Manchester City (ENG)",
+    "Wydad AC (MAR)",
+    "Al Ain FC (UAE)",
+    "Juventus FC (ITA)"
+  ],
+  "Group H": [
+    "Real Madrid CF (ESP)",
+    "Al Hilal SFC (KSA)",
+    "CF Pachuca (MEX)",
+    "FC Salzburg (AUT)"
+  ]
+};
+
 export default function ClubWorldCup() {
   return (
     <div className="club-world-cup-page">
@@ -14,13 +65,18 @@ export default function ClubWorldCup() {
 
       <section className="section">
         <h2>Participating Teams</h2>
-        <ul className="team-list">
-          <li>Manchester City (UEFA)</li>
-          <li>Flamengo (CONMEBOL)</li>
-          <li>Al Ahly (CAF)</li>
-          <li>Seattle Sounders (CONCACAF)</li>
-          {/* Add more teams later via data */}
-        </ul>
+        <div className="grouped-teams">
+          {Object.entries(groupedTeams).map(([group, teams]) => (
+            <div key={group} className="group-box">
+              <h3>{group}</h3>
+              <ul className="team-list">
+                {teams.map((team, index) => (
+                  <li key={index}>{team}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </section>
 
       <section className="section">
